a182cfa430e9c480c74b4a1bd79359c3358d4f2a
SHIMIZU Taku <shimizu.taku@gmail.com>
2013-01-19 12:07:39 +0900

    add install setting

--- apt-setup.sh
+++ apt-setup.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 
 # updateする前にしておきたいこと
-sudo aptitude -y install ssh
+sudo aptitude -y install ssh;
 
 # 日本語設定
-sudo locale-gen ja_JP.UTF-8
-sudo dpkg-reconfigure locales
+sudo aptitude install -y language-pack-ja;
+sudo dpkg-reconfigure locales;
 
-# とりあえずアップデート
-sudo aptitude update;
+# アップデート
+sudo aptitude update -y;
 sudo aptitude upgrade -y;
-sudo aptitude update;
 
 cat >> ~/.bash_profile <<EOF
 if [ -f ~/.bashrc ] ; then #.bashrcがあるならそっちをつかう
@@ -23,11 +22,20 @@ sudo aptitude install -y ntp;
 sudo aptitude install -y vim;
 sudo aptitude install -y git;
 sudo aptitude install -y tree;
-sudo aptitude install -y tig;
-sudo aptitude install -y htop;
 sudo aptitude install -y tmux;
+
+sudo aptitude install -y build-essential;
+sudo aptitude install -y libsqlite3-dev;
+sudo aptitude install -y libreadline6-dev;
+sudo aptitude install -y libgdbm-dev;
+sudo aptitude install -y zlib1g-dev;
+sudo aptitude install -y libbz2-dev;
 sudo aptitude install -y sqlite3;
-sudo aptitude install -y ctags;
+sudo aptitude install -y tk-dev;
+sudo aptitude install -y zip;
+
+# python関係のインストール
+sudo aptitude install -y python-dev;
 
 # NeoBundleするための準備
 mkdir ~/.vim/bundle;
