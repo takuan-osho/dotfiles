1bff3dc8fbb86cf6f728310364b0d017ee226fd0
SHIMIZU Taku <shimizu.taku@gmail.com>
2013-01-19 15:51:15 +0900

    change the place of dotfiles

--- bash_setup.sh
+++ bash_setup.sh
@@ -2,8 +2,20 @@
 
 DOT_FILES=( .vimrc .bashrc .zshrc .gitconfig .gitignore_global .hgrc .hgignore_global)
 
+mv dotfiles/ .dotfiles
+
 for file in ${DOT_FILES[@]}
   do
-    ln -s $HOME/dotfiles/$file $HOME/$file
-    echo "シンボリックリンクを貼りました: $file"
+    if [ -a $HOME/$file ]; then
+      if [ -L $HOME/$file  ]; then
+        echo "既にシンボリックリンクが存在します": $file
+      elif [ -d $HOME/$file ]; then
+        echo "既にディレクトリが存在します": $file
+      else
+        echo "既にファイルが存在します": $file
+      fi
+    else
+      ln -s $HOME/.dotfiles/$file $HOME/$file
+      echo "シンボリックリンクを貼りました: $file"
+    fi
   done
