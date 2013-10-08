fa2fc4b289ffb8221836e11249e454e56e06c959
SHIMIZU Taku <shimizu.taku@gmail.com>
2013-10-08 14:15:41 +0900

    golang用の設定追加

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -26,3 +26,17 @@
 
 ;; インサートステートでemacsとして足りないキーバインドを追加する。
 (define-key evil-insert-state-map (kbd "C-e") 'move-end-of-line)
+
+;; golang用の設定
+(add-to-list 'load-path "~/.emacs.d/elpa/golang")
+(require 'go-mode-load)
+(add-hook 'go-mode-hook
+          '(lambda()
+            (setq c-basic-offset 4)
+            (setq indent-tabs-mode t)
+            (local-set-key (kbd "M-.") 'godef-jump)
+            (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)
+            (local-set-key (kbd "C-c i") 'go-goto-imports)
+            (local-set-key (kbd "C-c d") 'godoc)))
+
+(add-hook 'before-save-hook 'gofmt-before-save)
